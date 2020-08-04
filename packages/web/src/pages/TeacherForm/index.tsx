ae9568aba20a599c5eebeb058298f44765ebfaf5
pedroksty <pedrotrab2132@gmail.com>
2020-08-04 01:36:35 -0300

    Add study page

--- packages/web/src/pages/TeacherForm/index.tsx
+++ packages/web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-// import { Container } from './styles';
+import PageHeader from '../../components/Pageheader'
+
+import './styles.css'
 
 const TeacherForm: React.FC = () => {
-  return <h1>Teacher form</h1>
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </div>
+  )
 }
 
 export default TeacherForm
--- packages/web/src/pages/TeacherList/index.tsx
+++ packages/web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-// import { Container } from './styles';
+import logoImg from '../../assets/images/logo.svg'
+import backIcon from '../../assets/images/icons/back.svg'
+
+import './styles.css'
+
+import Pageheader from '../../components/Pageheader'
 
 const TeacherList: React.FC = () => {
-  return <h1>Teacher List</h1>
+  return (
+    <div id="page-teacher-list" className="container">
+      <Pageheader title="Esses são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Dia da semana</label>
+            <input type="text" id="week-day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </Pageheader>
+    </div>
+  )
 }
 
 export default TeacherList
