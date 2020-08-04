d2ea81e377d283dd229c3258a8a6eb4ee8c06dcd
pedroksty <pedrotrab2132@gmail.com>
2020-08-04 18:43:59 -0300

    Add transaction to user route

--- packages/server/src/controllers/UserController.ts
+++ packages/server/src/controllers/UserController.ts
@@ -20,40 +20,52 @@ class UserController {
       schedule
     } = request.body
 
-    const insertedUserIds = await db('users')
-      .insert({
-        name,
-        avatar,
-        bio,
-        whatsapp
+    const trx = await db.transaction()
+
+    try {
+      const insertedUserIds = await trx('users')
+        .insert({
+          name,
+          avatar,
+          bio,
+          whatsapp
+        })
+        .returning('*')
+
+      const user_id = insertedUserIds[0].id
+      const insertedClassId = await trx('classes')
+        .insert({
+          subject,
+          cost,
+          user_id
+        })
+        .returning('*')
+
+      const class_id = insertedClassId[0].id
+
+      const classSchdule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+          class_id,
+          week_day: scheduleItem.week_day,
+          from: convertHoursToMinute(scheduleItem.from),
+          to: convertHoursToMinute(scheduleItem.to)
+        }
       })
-      .returning('*')
 
-    const user_id = insertedUserIds[0].id
-    const insertedClassId = await db('classes')
-      .insert({
-        subject,
-        cost,
-        user_id
+      const insertedClass = await trx('class_schedule')
+        .insert(classSchdule)
+        .returning('*')
+
+      await trx.commit()
+
+      return response.json({ insertedUserIds, insertedClassId, insertedClass })
+    } catch (err) {
+      await trx.rollback()
+      console.log(err)
+      return response.status(400).json({
+        error: 'Unexpected error while creating new class'
       })
-      .returning('*')
-
-    const class_id = insertedClassId[0].id
-
-    const classSchdule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHoursToMinute(scheduleItem.from),
-        to: convertHoursToMinute(scheduleItem.to)
-      }
-    })
-
-    const insertedClass = await db('class_schedule')
-      .insert(classSchdule)
-      .returning('*')
-
-    return response.json({ insertedUserIds, insertedClassId, insertedClass })
+    }
   }
 
   async index(request: Request, response: Response): Promise<any> {}
